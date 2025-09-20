c8456fdf2feba22842eff01b20d1d6d7c3ab2a73
Emblemdevice <noorulzoha93@gmail.com>
2025-09-20 22:54:45 +0500

    query

---
+++ query.sql
@@ -0,0 +1,19 @@
+USE parks_and_recreation;
+
+SELECT pd.department_id,
+       pd.department_name,
+        COUNT(ed.employee_id) AS employee_count
+       
+FROM employee_demographics ed
+JOIN employee_salary es
+      ON ed.employee_id = es.employee_id
+  
+JOIN parks_departments pd
+ ON es.dept_id = pd.department_id
+ 
+ GROUP BY pd.department_id;
+ 
+
+ 
+
+      
